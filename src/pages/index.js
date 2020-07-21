2a0eafc1614d674560d757baebbeb7b7f6008ebb
mbutynski2 <64858721+mbutynski2@users.noreply.github.com>
2020-07-21 14:50:17 -0500

    change title of of video  button

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi. Nice to meet y ou.</h1>
+    <p>Welcome our UX process and portfolio site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Watch the video</Link>
   </Layout>
 )
 
 export default IndexPage
+
