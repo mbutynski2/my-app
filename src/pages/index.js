09f6eb910c1afbe749f86b2f1420c053fea9d30e
mbutynski2 <64858721+mbutynski2@users.noreply.github.com>
2020-07-22 17:50:19 -0500

    Links, process, contact us

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,57 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <h1>User Experience is essential for digital transformation</h1>
-        <p>Get to know the UX team, our process, <br />and some of our latest works from the New Orleans Digital Delivery Center.</p>
+        <p>Get to know the UX team, our process, <br />and see some of our latest works from the New Orleans Digital Delivery Center.</p>
         <div>
         <Link to="/using-typescript/">Portfolio</Link>
         </div>
+      </div>
+    </div>
+    <div className="Title">
+      <h1>Meet the Team</h1>
+      <div className="MeetGroup">
+        <div>
+          <Link to="/"><img src={require ('../images/logo.svg')} width="150" /></Link>
+          <h2>Mandi Butynski</h2>
+          <h3>UX/ UI Capability Lead</h3>
+          <p><span className="Underline"><Link to="/">Learn more</Link></span></p>
+        </div>
+        <div>
+          <Link to="/"><img src={require ('../images/logo.svg')} width="150" /></Link>
+          <h2>Katie Riles</h2>
+          <h3>Sr. UX Designer</h3>
+          <p><span className="Underline"><Link to="/">Learn more</Link></span></p>
+        </div>
+        <div>
+          <Link to="/"><img src={require ('../images/logo.svg')} width="150" /></Link>
+          <h2>Mansi Sundriyal</h2>
+          <h3>Sr. UX/UI Developer</h3>
+          <p><span className="Underline"><Link to="/">Learn more</Link></span></p>
+        </div>
+        <div>
+          <Link to="/"><img src={require ('../images/logo.svg')} width="150" /></Link>
+          <h2>Merilly Ruglas</h2>
+          <h3>AV/VR Designer</h3>
+          <p><span className="Underline"><Link to="/">Learn more</Link></span></p>
+        </div>
+        <div>
+          <Link to="/"><img src={require ('../images/logo.svg')} width="150" /></Link>
+          <h2>Vicky Tran</h2>
+          <h3>UX/UI Developer</h3>
+          <p><span className="Underline"><Link to="/">Learn more</Link></span></p>
+        </div>
+      </div>
+    </div>
+    <div className="OurProcess">
+      <div className="Title">
+        <h1>Our Process</h1>
+        <p>Together we have crafted a method of delivering exeptional user experiences that drive results and delight.</p>
+      </div>
+    </div>
+    <div className="ContactUs">
+      <div className="Title">
+        <h1>Contact US</h1>
+        <p>Email us at<span className="Underline"><Link to="/">uxui@dxcdtc.com</Link></span>Hope to hear from you soon!</p>
       </div>
     </div>
     
